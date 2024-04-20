99713a7bfcf813c7fd8b535c9a3008bfc9be9dce
Sarterin <sarterion137@gmail.com>
2024-04-20 16:56:24 +0300

    Add routing

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,9 +1,10 @@
 import { BuildOptions } from "./types/config";
-import { Configuration } from "webpack-dev-server";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
-export function buildDevServer(options: BuildOptions): Configuration {
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
   return {
     port: options.port,
-    open: true
+    open: true,
+    historyApiFallback: true
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { Suspense } from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 
 const App = () => {
   return (
       <div className="app">
-        Test
+        <Link to={'/'}>Main</Link>
+        <Link to={'/about'}>About</Link>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path={'/'} element={<MainPageAsync />} />
+              <Route path={'/about'} element={<AboutPageAsync />} />
+            </Routes>
+          </Suspense>
       </div>
   );
 };
